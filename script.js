dc602182030bfeb46d8fe413daf7819a39a1316f
Magnus0155 <magnuseeg1@gmail.com>
2024-12-12 14:00:12 +0100

    123

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ console.log(inputField,addButton,todoList);
 addButton.addEventListener("click", function () {
     console.log("legg til knappen er trykket!");
 
-// lager et nytt element? inputFieldValue. Etterpå lagres verdien fra inputField til inputfieldValue med å bruke .value .
+// lager et nytt sted for å lagre en verdi for inputField.
     const inputFieldValue = inputField.value;
     console.log(inputFieldValue);
 
-// Lager et nytt element todoItem. Etterpå lages texsten som blir skrevet i textbokesen. 
+// Lager et nytt element todoItem og ("li") gjør den et kiste element.
+// Etterpå stter vi en text verdi til todoItem som er verdien til inputFieldValue
+// TodoItem blir så lagt til todoList med appendChild
+// Det siste som gjøres er å sette en ingen verdi på inputFIeld, for at brukeren ikke må manuelt slette teksten selv etter at de har skrevet noe.
     const todoItem = document.createElement("li");
     todoItem.textContent = inputFieldValue;
     todoList.appendChild(todoItem);
@@ -21,8 +24,9 @@ addButton.addEventListener("click", function () {
 
 
 
-    //lag en variabel som lager en knapp for ferdig
-//laget et nytt element. Det er en knapp som sier ferdig. Markerer det som som er lagt til for ferdig. 
+// Laget en variabel som lager en knapp for ferdig.
+// Etterpå lages det om og eller uttalelse for ferdig knappen.
+// Dette gjør at knappen kan bli markert som ferdig.
     const doneItem = document.createElement("button");
     doneItem.textContent = "Done";
     todoItem.appendChild(doneItem);
@@ -36,8 +40,8 @@ addButton.addEventListener("click", function () {
         }
     });
 
-/* Laget et nytt element. Slett knappen sletter det som er markert ferdig.
-/  Hvis ikke viser det en fane om at ferdig knappen må trykkes*/
+/*Laget en ny variabel. Slett knappen sletter det som er markert ferdig.
+  Hvis ikke viser det en fane om at ferdig knappen må trykkes*/
     const deleteItem = document.createElement("button");
     deleteItem.textContent = "Slett";
     todoItem.appendChild(deleteItem);
